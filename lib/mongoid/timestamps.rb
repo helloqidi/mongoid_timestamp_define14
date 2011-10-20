617677140c3bc02f79e05f00e7a9f3d4d59f556c
Helloqidi <helloqidi@gmail.com>
2011-10-20 16:30:23 +0800

    change created_at to created_on..

---
+++ lib/mongoid/timestamps.rb
@@ -0,0 +1,20 @@
+# encoding: utf-8
+require "mongoid/timestamps/created"
+require "mongoid/timestamps/updated"
+require "mongoid/timestamps/timeless"
+
+require "mongoid/timestamps/created_on"
+require "mongoid/timestamps/updated_on"
+
+module Mongoid #:nodoc:
+
+  # This module handles the behaviour for setting up document created at and
+  # updated at timestamps.
+  module Timestamps
+    extend ActiveSupport::Concern
+    include Created
+    include Updated
+    include CreatedOn
+    include UpdatedOn
+  end
+end
